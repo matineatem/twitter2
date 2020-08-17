9a9519d0292f34ae0d7d862c577a6660826de0fa
pennd44 <pdiba@oxbowschool.org>
2020-08-17 15:04:48 -0400

    push

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -8,15 +8,20 @@ class LikesController < ApplicationController
   end
 
   def create
-    
     #params[:like][:user_id, :tweet_id]
     #   Like.create(user_id: .id, tweet_id: tweet.id)
-    @like = Like.create(like_params)
+
+    
+    @like = Like.create(user_id: Tweet.find(params[:format]).user.id, tweet_id: Tweet.find(params[:format]).id)
+    #byebug
+
+    #@like = Like.create(like_params)
+
   end
 
 
   private
   def like_params
-    params.require(:like).permit(:user, :tweet)
+    params.require(:like).permit(:user_id, :tweet_id)
   end
 end
